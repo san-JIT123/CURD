f6a933241700c26d5df0c2882b0e92b936e278ac
Sanji <your-email@gmail.com>
2026-05-27 19:45:46 +0530

    variants-2 fetch and update  note api  complete

--- variants-2/src/app.js
+++ variants-2/src/app.js
@@ -31,6 +31,42 @@ app.post("/api/notes", async (req, res) => {
   });
 });
 
+// fetch api
 
-// 
+app.get("/api/notes", async (req, res) => {
+  let notes = await NoteModel.find();
+
+  return res.status(200).json({
+    message: "fetch note successfully",
+    note: notes,
+  });
+});
+// updated note api
+app.patch("/api/notes/:id", async (req, res) => {
+  let { id } = req.params;
+  let { title, des } = req.body;
+
+  if (!des) return res.status(400).json({ error: "Description is required" });
+
+  if (des.trim().length < 10)
+    return res.status(400).json({
+      error: "title required 10 character long",
+    });
+
+  let note = await NoteModel.findById(id);
+
+  if (!note)
+    return res.status(404).json({
+      error: "Notes note found",
+    });
+
+  note.des = des;
+  await note.save();
+
+  return res.status(200).json({
+    message: "Update note Successfully",
+    updateNote: note,
+  });
+});
+
 export default app;
