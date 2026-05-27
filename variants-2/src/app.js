038b99c510297365bcc8c5642a98667b32101973
Sanji <your-email@gmail.com>
2026-05-27 19:35:36 +0530

    variants-2 create note api  complete

--- variants-2/src/app.js
+++ variants-2/src/app.js
@@ -4,4 +4,33 @@ import NoteModel from "./model/notes.model.js";
 let app = express();
 app.use(express.json());
 
+// create notes api
+app.post("/api/notes", async (req, res) => {
+  let { title, des } = req.body;
+
+  if (!title || !des)
+    return res.status(400).json({ error: "All Field are required" });
+
+  if (title.trim().length < 3)
+    return res.status(400).json({
+      error: "title required 3 character long",
+    });
+  if (des.trim().length < 10)
+    return res.status(400).json({
+      error: "title required 10 character long",
+    });
+
+  let newNote = await NoteModel.create({
+    title,
+    des,
+  });
+
+  return res.status(201).json({
+    message: "Note create successfully",
+    note: newNote,
+  });
+});
+
+
+// 
 export default app;
