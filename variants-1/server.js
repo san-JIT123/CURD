9e276cee65372ed5fd038ddc7e72b4b329408d98
Sanji <your-email@gmail.com>
2026-05-27 18:28:10 +0530

    setup express and mongoose

--- variants-1/server.js
+++ variants-1/server.js
@@ -1,9 +1,4 @@
-// server start karna
-// connecting with mongoose
-
 import app from "./src/app.js";
-import { connectDB } from "./src/config/db.js";
-
-console.log("ram");
+import connectDB from "./src/config/database.js";
 await connectDB();
-app.listen(3000, () => console.log("server ise working...3000"));
+app.listen(3000, () => console.log("server  is working....3000"));
---
+++ variants-1/src/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+let connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://kodex:12345san@cluster1.ilo46hx.mongodb.net/CURD",
+    );
+    console.log('connectDB....')
+  } catch (error) {
+    console.log("mongoose error", error);
+  }
+};
+
+export default connectDB;
